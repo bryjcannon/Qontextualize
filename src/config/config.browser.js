377efecf0d1619de844b3f6d775f6ea1a0af2132
Bryan Cannon <bryjcannon@gmail.com>
2025-04-02 15:58:22 -0700

    first render working version

--- src/config/config.browser.js
+++ src/config/config.browser.js
@@ -1,6 +1,6 @@
 // Browser-specific configuration
 const config = {
-    PROXY_API_ENDPOINT: 'https://api.qontextualize.com/api/analyze',
+    PROXY_API_ENDPOINT: 'https://render-api.qontextualize.com/api/analyze',
     MAX_RETRIES: 3,
     RETRY_DELAY: 1000,
 };
